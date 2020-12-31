eb6db226acaac2a5300535712af9924bebdcfbaf
Andy Klimczak <andyklimczak@fastmail.com>
2020-12-30 22:53:35 -0500

    Set isLooping to false to try to fix stutter

--- src/stores/RadioStore.js
+++ src/stores/RadioStore.js
@@ -38,6 +38,7 @@ export default class RadioStore {
                 {uri: station.url},
                 {
                     shouldPlay: true,
+                    isLooping: false,
                     volume: this.volume,
                     progressUpdateIntervalMillis: 500,
                 }
